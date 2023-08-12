a18070e9b51f20c87ad1f83d4a928c8cd3916da9
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2023-08-12 10:49:23 +0300

    add MPAndroidChart dependency

--- app/src/main/java/com/example/weather/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/weather/ui/home/HomeFragment.kt
@@ -1,18 +1,26 @@
 package com.example.weather.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.weather.R
+import androidx.fragment.app.Fragment
+import com.example.weather.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(LayoutInflater.from(context), container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
