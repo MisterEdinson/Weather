2fb7e5ccdfb5c532fa5a8838cf358ce2e0fada62
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2023-08-13 14:35:31 +0300

    config layout
    add spinner adapters

--- app/src/main/java/com/example/weather/ui/config/ConfigFragment.kt
+++ app/src/main/java/com/example/weather/ui/config/ConfigFragment.kt
@@ -8,12 +8,24 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.weather.R
 import com.example.weather.databinding.FragmentConfigBinding
+import com.example.weather.ui.config.adapters.AdapterLangConfig
+import com.example.weather.ui.config.adapters.AdapterPrecipitationConfig
+import com.example.weather.ui.config.adapters.AdapterPressureConfig
+import com.example.weather.ui.config.adapters.AdapterTempConfig
+import com.example.weather.ui.config.adapters.AdapterTimeConfig
+import com.example.weather.ui.config.adapters.AdapterWindConfig
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.NonDisposableHandle.parent
 @AndroidEntryPoint
 class ConfigFragment : Fragment() {
 
     private lateinit var binding: FragmentConfigBinding
+    private var adapterTemp: AdapterTempConfig? = null
+    private var adapterTime: AdapterTimeConfig? = null
+    private var adapterPrecipitation: AdapterPrecipitationConfig? = null
+    private var adapterWind: AdapterWindConfig? = null
+    private var adapterPressure: AdapterPressureConfig? = null
+    private var adapterLang: AdapterLangConfig? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,8 +37,29 @@ class ConfigFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initAdapter()
         binding.btnSaveConfig.setOnClickListener{
             findNavController().navigate(R.id.action_configFragment_to_homeFragment)
         }
+    }
+
+    private fun initAdapter(){
+        adapterTemp = AdapterTempConfig(this.context)
+        binding.spConfigTemp.adapter = adapterTemp
+
+        adapterTime = AdapterTimeConfig(this.context)
+        binding.spConfigTime.adapter = adapterTime
+
+        adapterPrecipitation = AdapterPrecipitationConfig(this.context)
+        binding.spConfigPrecipitation.adapter = adapterPrecipitation
+
+        adapterWind = AdapterWindConfig(this.context)
+        binding.spConfigWind.adapter = adapterWind
+
+        adapterPressure = AdapterPressureConfig(this.context)
+        binding.spConfigPressure.adapter = adapterPressure
+
+        adapterLang = AdapterLangConfig(this.context)
+        binding.spConfigLanguage.adapter = adapterLang
     }
 }
