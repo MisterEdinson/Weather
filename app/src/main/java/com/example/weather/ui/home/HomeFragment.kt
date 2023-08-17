a77dba797f7447b9d8880303a2603383980f2eba
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2023-08-17 13:13:06 +0300

    converted time and date

--- app/src/main/java/com/example/weather/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/weather/ui/home/HomeFragment.kt
@@ -6,9 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.weather.R
 import com.example.weather.databinding.FragmentHomeBinding
 import com.example.weather.ui.home.converters.Condition
+import com.example.weather.ui.home.converters.TimeConvertedUnix
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -28,28 +30,38 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.weatherLive.observe(viewLifecycleOwner) {
-            binding.tvHeaderLoacation.text = getString(
-                R.string.location_show,
-                it.geoObject?.locality?.name,
-                it.geoObject?.country?.name
-            )
+            binding.apply {
+                tvHeaderLoacation.text = getString(
+                    R.string.location_show,
+                    it.geoObject?.locality?.name,
+                    it.geoObject?.country?.name
+                )
 
-            binding.mainBanner.tvNightDayTemp.text = getString(
-                R.string.temp_naught_day,
-                it.forecasts?.get(0)?.parts?.night?.tempMax,
-                it.forecasts?.get(0)?.parts?.day?.tempMax
-            )
-            binding.mainBanner.tvActyalTemp.text = getString(R.string.temp_show, it.fact?.temp)
-            binding.mainBanner.tvVectorWind.text =
-                getString(R.string.vector_show, it.fact?.windDir).uppercase()
-            binding.mainBanner.tvSpeedWind.text = getString(R.string.speed_show, it.fact?.windSpeed)
-            binding.mainBanner.tvHumidity.text =
-                getString(R.string.humidity_show, it.fact?.humidity)
-            binding.mainBanner.tvActyalWeather.text = Condition().converted(it.fact?.condition)
+                mainBanner.tvNightDayTemp.text = getString(
+                    R.string.temp_naught_day,
+                    it.forecasts?.get(0)?.parts?.night?.tempMax,
+                    it.forecasts?.get(0)?.parts?.day?.tempMax
+                )
+                mainBanner.apply {
+                    tvActyalTemp.text = getString(R.string.temp_show, it.fact?.temp)
+                    tvVectorWind.text = getString(R.string.vector_show, it.fact?.windDir).uppercase()
+                    tvSpeedWind.text = getString(R.string.speed_show, it.fact?.windSpeed)
+                    tvHumidity.text = getString(R.string.humidity_show, it.fact?.humidity)
+                    tvActyalWeather.text = Condition().converted(it.fact?.condition)
+
+                    val tz = (it.info?.tzinfo?.offset ?: 0) + (it.fact?.uptime ?: 0)
+                    tvTimeHome.text = TimeConvertedUnix().time(tz)
+                    tvDateHome.text = TimeConvertedUnix().converted(tz)
+                }
+            }
         }
 
         binding.imgBtnBar.setOnClickListener {
             viewModel.drawerLayoutState.value = true
+        }
+
+        binding.imgBtnLocation.setOnClickListener {
+            findNavController().navigate(R.id.nav_location)
         }
     }
 }
