09ab6de1faf5c6df984784154954925089a42f96
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2023-08-17 09:50:55 +0300

    navigate info fragment

--- app/src/main/java/com/example/weather/ui/infoApp/InfoFragment.kt
+++ app/src/main/java/com/example/weather/ui/infoApp/InfoFragment.kt
@@ -5,13 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.weather.R
+import com.example.weather.databinding.FragmentInfoBinding
+
 class InfoFragment : Fragment() {
+
+    private lateinit var binding: FragmentInfoBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_info, container, false)
+        binding = FragmentInfoBinding.inflate(LayoutInflater.from(context), container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.imgCloseInfo.setOnClickListener {
+            findNavController().navigate(R.id.nav_home)
+        }
     }
 }
